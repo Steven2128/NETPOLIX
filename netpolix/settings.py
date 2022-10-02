6ba4060af874888e09d61c830ba505b530e2b143
Richard Stephen Barboza Alarcón <rbarboza@tredasolutions.com>
2022-10-02 13:38:24 -0500

    feat: Agrega documentación de las api con swagger

--- netpolix/settings.py
+++ netpolix/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'users',
     'films',
 ]
@@ -69,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'staticfiles': 'django.templatetags.static',
+            }
         },
     },
 ]
